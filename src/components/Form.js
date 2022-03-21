e3f47ca0ee24c908bdcfdeefb94326831bca1d26
Dishant <taredishant21@gmail.com>
2022-03-21 20:55:14 +0530

    fixed bugs

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef, useContext } from 'react'
 // import isURL from 'validator/lib/isURL';
 import Result from './Result';
 import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
 const Form = ({fetchUrls, urls, setUrls}) => {
+    const api = process.env.REACT_APP_HOSTED_BASE_URL;
     const [longUrl, setLongUrl] = useState('');
     const [shortUrl, setShortUrl] = useState('');
     const [disabled, setDisabled] = useState(false);
     const [loading, setLoading] = useState(false);
     const [urlDesc, setUrlDesc] = useState('');
     const [copy, setCopy] = useState(true);
+
+    const {user} = useContext(AuthContext);
 
     useEffect(() => {
 
@@ -17,7 +21,7 @@ const Form = ({fetchUrls, urls, setUrls}) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setDisabled(true);
-        const response = await axios.post('http://localhost:5000/shorten', { userId: '622cc236b059a4da868ccc1a', longUrl: longUrl, urlDescription: urlDesc })
+        const response = await axios.post(`${api}shorten`, { userId: user._id, longUrl: longUrl, urlDescription: urlDesc })
         const data = response.data;
         console.log(data);
         setShortUrl(data.shortUrl)
--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const TableRows = ({ long, short, date, clicks, urlDescription }) => {
+    const api = process.env.REACT_APP_HOSTED_BASE_URL;
     return (
         <tr>
             <td><a href={long} target="_blank" rel="noreferrer">{long}</a> <br /></td>
-            <td><a href={`http://localhost:5000/${short}`} target="_blank" rel="noreferrer">localhost:5000/{short}</a></td>
+            <td><a href={`${api}${short}`} target="_blank" rel="noreferrer">shtme.herokuapp.com/{short}</a></td>
             <td>{clicks} clicks</td>
             <td>{urlDescription ? urlDescription : '-' }</td>
         </tr>
